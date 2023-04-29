aa355f2d2913354081b0a9d516c0c4836e81f436
Pablo Gomez <pablichgg@gmail.com>
2023-04-29 15:00:26 +0200

    wip

--- app/src/main/java/com/example/practica_fundamentos_android/login/LoginActivity.kt
+++ app/src/main/java/com/example/practica_fundamentos_android/login/LoginActivity.kt
@@ -1,19 +1,25 @@
 package com.example.practica_fundamentos_android.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.practica_fundamentos_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel : LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button
+        binding.button.setOnClickListener {
+            viewModel.email = binding.email.text.toString()
+            viewModel.password = binding.password.text.toString()
+            viewModel.login()
+        }
 
     }
 }
--- app/src/main/java/com/example/practica_fundamentos_android/login/LoginViewModel.kt
+++ app/src/main/java/com/example/practica_fundamentos_android/login/LoginViewModel.kt
@@ -1,8 +1,25 @@
 package com.example.practica_fundamentos_android.login
 
 import androidx.lifecycle.ViewModel
+import com.example.practica_fundamentos_android.network.Network
 
 class LoginViewModel: ViewModel() {
 
+    private val network: Network = Network()
+
+    lateinit  var  email: String
+    lateinit  var  password: String
+    fun login(){
+       if (checkCredentials()) {
+
+       }
+       else {
+
+       }
+    }
+
+    fun checkCredentials() : Boolean{
+
+    }
 
 }
